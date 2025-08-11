4b7e80a6d652c5655607d03c1d2f21cd461b45cb
pvigelli <pvigelli@gmail.com>
2025-08-12 00:00:44 +0200

    Create MainActivity.java

---
+++ android/app/src/main/java/com/pvigelli/finlens/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pvigelli.finlens;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "Finlens";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
+    }
+}
